40c256abb9b7e623d6838ec6ad03cb08e0b9cdb5
DsciPaul-gg <paulashleydelacruz.26@gmail.com>
2024-01-15 22:19:53 +0800

    CC-102

---
+++ LabEx3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    int sqr;
+
+    while (x <= 15) {
+
+        sqr = x * x;
+
+        x+=3;
+
+        cout << sqr << endl;
+    }
+
+    return EXIT_SUCCESS;
+
+}
