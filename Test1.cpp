9dcebabe10f2c66bfeb665da03ca1c9b9a1b1d5c
DsciPaul-gg <paulashleydelacruz.26@gmail.com>
2024-01-19 22:33:51 +0800

    CC-102

---
+++ Test1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number = 0;
+    int sum = 0;
+   
+    do {
+        
+        sum += number;
+    
+        cout << "Enter a number: ";
+        cin >> number;
+
+    }   while (number >= 0);
+    
+    cout << endl << "The sum is " << sum << endl;
+    
+    return 0;
+}
