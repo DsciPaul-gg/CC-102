6921eb70b2f8cc19080cdc2b6e47e9e2b3330be4
DsciPaul-gg <paulashleydelacruz.26@gmail.com>
2024-01-28 19:59:23 +0800

    REPORTING

--- Test1.cpp
+++ Test1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -7,16 +8,18 @@ int main()
     int number = 0;
     int sum = 0;
    
-    do {
-        
-        sum += number;
+    cout << endl << "THIS LOOP ADDS POSITIVE NUMBERS CONTINUOUSLY, NOT UNTIL YOU ENTER A NEGATIVE NUMBER" << endl;
     
+    do 
+    {
         cout << "Enter a number: ";
         cin >> number;
+
+        sum += number;
 
     }   while (number >= 0);
     
     cout << endl << "The sum is " << sum << endl;
     
-    return 0;
+    return EXIT_SUCCESS;
 }
